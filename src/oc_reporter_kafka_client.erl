9520341c5132558033d521f1e717d2229194e1cb
Mike.Bearne <mike@mike-mac.local>
2020-01-09 08:29:20 +0000

    change to map one span to kafka msg

--- src/oc_reporter_kafka_client.erl
+++ src/oc_reporter_kafka_client.erl
@@ -15,18 +15,32 @@ start_link(Endpoints, Topic, ClientId, ProducerConfig) ->
     ok = brod:start_producer(ClientId, Topic, ProducerConfig),
     {ok, []}.
 
+% report_spans(Spans, ClientId, Topic, Partitioner, Headers) ->
+%     [FirstSpan|_rest] = Spans,
+%     TraceId = maps:get(trace_id,FirstSpan),
+%     Encoded = dump_pb:encode_msg(#{spans => Spans}, dump_spans),
+%     HeadersParsed = case Headers of
+%       {Module, Fun} -> erlang:apply(Module, Fun, []);
+%       nil -> [];
+%       _Else -> []
+%     end,
+%     Msg = #{ts => brod_utils:epoch_ms(), value => Encoded, headers => HeadersParsed},
+%     {ok, _FirstOffset} = brod:produce_sync_offset(ClientId, Topic, Partitioner, TraceId, Msg).
+%     % {ok, _FirstOffset} = brod:produce_sync_offset(ClientId, Topic, Partitioner, TraceId, Encoded).
+
 report_spans(Spans, ClientId, Topic, Partitioner, Headers) ->
-    [FirstSpan|_rest] = Spans,
-    TraceId = maps:get(trace_id,FirstSpan),
-    Encoded = dump_pb:encode_msg(#{spans => Spans}, dump_spans),
     HeadersParsed = case Headers of
       {Module, Fun} -> erlang:apply(Module, Fun, []);
       nil -> [];
       _Else -> []
     end,
-    Msg = #{ts => brod_utils:epoch_ms(), value => Encoded, headers => HeadersParsed},
-    {ok, _FirstOffset} = brod:produce_sync_offset(ClientId, Topic, Partitioner, TraceId, Msg).
-    % {ok, _FirstOffset} = brod:produce_sync_offset(ClientId, Topic, Partitioner, TraceId, Encoded).
+    lists:foreach(
+      fun (Span) ->
+          TraceId = maps:get(trace_id,Span),
+          Encoded = trace_service_pb:encode_msg(Span, span),
+          Msg = #{ts => brod_utils:epoch_ms(), value => Encoded, headers => HeadersParsed},
+          ok = brod:produce_no_ack(ClientId, Topic, Partitioner, TraceId, Msg)
+      end, Spans).
 
 parse_bootstrap_servers(Endpoints) when is_binary(Endpoints) -> 
   Urls = string:split(Endpoints, ","),
